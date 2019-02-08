f96d8d65b372c0636bc9671648359c3b195b2c11
Björn Zeeb <zabcom@users.noreply.github.com>
2019-02-08 09:16:36 -0500

    Rename parent_dev to sim_dev.
    Suggested by imp in D12467.

--- sys/cam/cam_sim.h
+++ sys/cam/cam_sim.h
@@ -109,7 +109,7 @@ struct cam_sim {
 	struct callout		callout;
 	struct cam_devq 	*devq;	/* Device Queue to use for this SIM */
 	int			refcount; /* References to the SIM. */
-	device_t		parent_dev; /* To export to attached peripherals */
+	device_t		sim_dev; /* For attached peripherals. */
 };
 
 #define CAM_SIM_LOCK(sim)	mtx_lock((sim)->mtx)
